95fb2fab684eb5a33db5f91209a4d278bee860e7
Gill Abada <gill@callifi.com>
2019-04-10 17:17:34 -0400

    test controls component.

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -5,11 +5,34 @@ import 'jest-dom/extend-expect';
 import 'react-testing-library/cleanup-after-each';
 
 import Controls from './Controls';
+// import Dashboard from '../dashboard/Dashboard';
+import { fireEvent } from 'react-testing-library/dist';
 
 describe('<Controls />', () => {
   it('component renders', () => {
     render(<Controls />);
   });
+  describe('buttons', () => {
+    it('provide buttons to toggle the closed and locked states', () => {
+      const { getByText } = render(<Controls />);
+      getByText(/close gate/i);
+      getByText(/lock gate/i);
+    });
+    it('text changes to reflect the state the door will be in if clicked', () => {
+      const { getByText } = render(<Controls closed={true} />);
+      getByText(/open gate/i);
+    });
+    it('the closed toggle button is disabled if the gate is closed', () => {
+      const { queryByText } = render(<Controls closed={true} />);
+      expect(queryByText(/close gate/i)).toBeNull();
+    });
+    it('the locked toggle button is disabled if the gate is open', () => {
+      const { queryByText } = render(<Controls />);
+      expect(queryByText(/lock gate/i).disabled).toBeTruthy();
+      expect(queryByText(/lock gate/i).enabled).toBeFalsy();
+    });
+  });
+
   describe('Gate', () => {
     it('defaults to unlocked and open', () => {
       const { getByText } = render(<Controls />);
